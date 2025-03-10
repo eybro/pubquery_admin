fc27b3b2dbb5a63a161843640aaa50e82322851b
eybro <erik.j.raberg@gmail.com>
2025-03-10 23:03:02 +0100

    middleware fix

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -26,5 +26,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: "/((?!_next/static|_next/image|favicon.ico|login).*)",
+  matcher: ["/dinners", "/((?!_next/static|_next/image|favicon.ico|login).*)"],
 };
