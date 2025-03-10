05b9bfe87d96a138deb8d5bcde42c05ee0fcb805
eybro <erik.j.raberg@gmail.com>
2025-03-10 22:23:25 +0100

    test

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -5,16 +5,20 @@ export function middleware(req: NextRequest) {
   const isLoginPage = req.nextUrl.pathname === "/login";
   const token = req.cookies.get("token")?.value;
 
+  console.log("Request to", req.nextUrl.pathname);
+
   if (token) {
     console.log("Token found:", token);
   }
 
 
   if (isLoginPage) {
+    console.log("Is login page");
     return NextResponse.next();
   }
 
   if (!token) {
+    console.log("No token found, redirecting to login page");
     return NextResponse.redirect("https://admin.pubquery.se/login");
   }
 
